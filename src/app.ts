e0fd47eb05bc4b79192fadacd7efc0296d147e8d
System Administrator <brunomarcio2006@gmail.com>
2022-06-22 19:11:58 -0300

    Revert "first commit"
    This reverts commit 570ddc9e2261e6d1a1701fb647450a5bc03e7d2f.

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,6 @@
-const anagrams = ['Awda', 'adwa']
-
-isAnagram(anagrams[0], anagrams[1], (result: boolean) => console.log(result))
-
-function isAnagram(
-    stringA: string, 
-    stringB: string,
-    result: any
-): void
-{
-    return result(cleanString(stringA) === cleanString(stringB))
+function sayHello(value: string) : number{
+    console.log(`hey ${value}`)
+    return 1
 }
 
-function cleanString(str: string): string{
-    return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
-  }
-
-
-
-
+sayHello('Brune')
